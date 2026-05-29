63efe94ce4b1564c7994f9009ee468cd6ec83229
Cronzooo <Lionel0025@gmail.com>
2026-05-29 14:44:39 -0400

    Add script to create the main resource group

---
+++ scripts/01-create-resource-group.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Create the main resource group for Cronzo Inc Azure Foundation
+# Usage: bash scripts/01-create-resource-group.sh
+
+az group create \
+  --name rg-cronzo-prod-eastus \
+  --location eastus \
+  --tags Environment=Lab Owner="Lionel Edoukou" Project=Cronzo-Foundation CostCenter=Learning
+  
